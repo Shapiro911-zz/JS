1689c1b46f94b7333160b6fb91e955beb3c7291d
Max <maxbui0911@gmail.com>
2021-07-01 17:58:43 +0300

    L2

--- JS_course/script.js
+++ JS_course/script.js
@@ -10,23 +10,78 @@ const products = [
     { id: 9, title: 'mango people t-shirt', price: 52.00 },
 ];
 
-const renderProduct = (product) => {
-    return `<div class="product_box">
-    <a class="link" href="single_page.html">
-        <img class="product_box_img" src="img/product_sort_img_${product.id}.png" alt="product_${product.id}">
-    <p class="text_box"> ${product.title}</p>
-        <p class="text_box text_box_pink">
-            $${product.price.toFixed(2)}</p>
-    </a>
-    <div class="box_add">
-        <a class="add" href="#"><img class="product_cart" src="img/cart.svg" alt="cart">
-            Add to Cart</a>
-    </div>
-    </div>`
+class GoodsItem {
+    constructor(id, title, price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `<div class="product_box">
+        <a class="link" href="single_page.html">
+            <img class="product_box_img" src="img/product_sort_img_${this.id}.png" alt="product_${this.id}">
+        <p class="text_box"> ${this.title}</p>
+            <p class="text_box text_box_pink">
+                $${this.price.toFixed(2)}</p>
+        </a>
+        <div class="box_add">
+            <a class="add" href="#"><img class="product_cart" src="img/cart.svg" alt="cart">
+                Add to Cart</a>
+        </div>
+        </div>`
+    }
 }
 
-const renderProductPage = (products) => {
-    document.querySelector('.product_list').innerHTML = products.map((product) => renderProduct(product)).join(' ');
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = products;
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.id, good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.product_list').innerHTML = listHtml;
+    }
+    calcPrice() {
+        let totalPrice = 0;
+        this.goods.forEach(good => {
+            totalPrice += good.price;
+        });
+        return totalPrice;
+    }
 }
 
-renderProductPage(products);
+class CartItem extends GoodsItem {
+    constructor(id, title, price, quantity) {
+        super(id, title, price);
+        this.quantity = quantity;
+    }
+}
+
+class Cart {
+    constructor() {
+        this.goods = [];
+    }
+    render() {
+
+    }
+    addItem() {
+
+    }
+    removeItem() {
+
+    }
+    calcPrice() {
+
+    }
+}
+
+const goodsList = new GoodsList();
+goodsList.fetchGoods();
+goodsList.render();
+console.log(goodsList.calcPrice());
